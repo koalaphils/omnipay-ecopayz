f396fd5dc4af88e77c675853f4fa4a4fb39dc0ff
Cydrick Nonog <cydrick.nonog@zmtsys.com>
2019-03-26 20:26:44 +0800

    Add api for getting setting

---
+++ src/ApiBundle/Controller/SettingController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ApiBundle\Controller;
+
+use AppBundle\Manager\SettingManager;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+
+class SettingController extends AbstractController
+{
+    /**
+     * @ApiDoc(
+     *     section="Application",
+     *     description="Setting/Configurations",
+     *     views={"piwi"}
+     * )
+     */
+    public function getSettingAction(Request $request, SettingManager $settingManager): View
+    {
+        $settings = [
+            'piwi247.session' => $settingManager->getSetting('piwi247.session'),
+            'bitcoin.setting' => $settingManager->getSetting('bitcoin.setting'),
+            'transaction.validate' => $settingManager->getSetting('transaction.validate'),
+        ];
+
+        return $this->view([
+            'error' => false,
+            'message' => '',
+            'status' => 200,
+            'data' => $settings,
+        ], 200);
+    }
+}
