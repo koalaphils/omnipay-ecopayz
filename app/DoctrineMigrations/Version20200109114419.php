9067611331ae3e93968a83f1fda36e86eea303c2
Bianca Boy <bianca.boy@zmtsys.com>
2020-01-09 20:23:35 +0800

    [PIW-145] Fix query

--- app/DoctrineMigrations/Version20200109114419.php
+++ app/DoctrineMigrations/Version20200109114419.php
@@ -13,7 +13,7 @@ final class Version20200109114419 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('UPDATE product SET product_name = ?', ['AFFILIATE PROFIT']);
+        $this->addSql('UPDATE product SET product_name = "AFFILIATE PROFIT" where product_id = 2');
 
     }
 
