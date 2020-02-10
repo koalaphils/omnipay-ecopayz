e60e1eb63fb60283c63513cf9ad801984582b33c
Jayfer <>
2020-02-10 17:42:52 +0800

    [PIW-271] added member request api

---
+++ src/ApiBundle/Manager/MemberRequestManager.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace ApiBundle\Manager;
+
+use AppBundle\Manager\AbstractManager;
+use Symfony\Component\HttpFoundation\Request;
+use DbBundle\Entity\Customer as Member;
+use ApiBundle\Repository\MemberRequestRepository;
+use DbBundle\Collection\Collection;
+use DbBundle\Entity\User;
+use DbBundle\Repository\UserRepository;
+
+class MemberRequestManager extends AbstractManager
+{
+
+    public function getMemberRequestList(Member $member, Request $request)
+    {
+        $filters = ['memberId' => $member->getId()];
+
+        $filters['limit'] = $request->get('limit', 10);
+        $filters['offset'] = (((int) $request->get('page', 1))-1) * $filters['limit'];
+        $orders = [];
+
+        if ($request->query->has('orders')) {
+            $orders = $request->query->get('orders');
+        } else {
+            $orders = [['column' => 'date', 'dir' => 'DESC']];
+        }
+
+        if ($request->query->has('sort')) {
+            $filters['sort'] = $request->query->get('sort');
+        }
+
+        if ($request->query->has('from')) {
+            $filters['from'] = convert_to_timezone($request->query->get('from'))->format('c');
+        }
+
+        if ($request->query->has('to')) {
+            $filters['to'] = convert_to_timezone($request->query->get('to'))->format('c');
+        }
+
+        if ($request->query->has('search')) {
+            $filters['search'] = $request->query->get('search');
+        }
+
+        if ($request->query->has('types')) {
+            $filters['types'] = explode(',', $request->query->get('types'));
+        }
+
+        if ($request->query->has('status')) {
+            $filters['status'] = $request->query->get('status');
+            if (!is_array($filters['status'])) {
+                $filters['status'] = [$filters['status']];
+            }
+        }
+
+        $requests = $this->getRepository()->filters($filters, $orders);
+        $total = $this->getRepository()->getTotal(['memberId' => $member->getId()]);
+        $totalFiltered = $this->getRepository()->getTotal($filters);
+        
+        return new Collection($requests, $total, $totalFiltered, $filters['limit'], $request->get('page', 1));
+    }
+
+    protected function getRepository(): MemberRequestRepository
+    {
+        return $this->getContainer()->get('api.member_request_repository');
+    }
+
+    protected function getUserRepository(): UserRepository
+    {
+        return $this->getDoctrine()->getRepository(User::class);
+    }
+}
