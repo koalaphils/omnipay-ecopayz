6f8c5eca7bb2a663183ffaf86cd24c8a9dc2a4ee
JC Frane <john.frane@zmtsys.com>
2020-03-11 21:29:19 +0800

    [ PIW-312 ][ FEATURE ] WIP.

--- src/ProductIntegrationBundle/DependencyInjection/ProductIntegrationExtension.php
+++ src/ProductIntegrationBundle/DependencyInjection/ProductIntegrationExtension.php
@@ -6,8 +6,8 @@ use ProductIntegrationBundle\Persistence\HttpPersistence;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
 
 class ProductIntegrationExtension extends Extension
@@ -22,6 +22,8 @@ class ProductIntegrationExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $integrations = $config['integrations'];
 
+        // Dynamically create Service Definitions for each integrations.
+        $factoryParameters = []; // Will be used by the factory
         foreach ($integrations as $key => $integration) {
             $arguments = $this->resolveArguments($container, $integration);
 
@@ -29,7 +31,13 @@ class ProductIntegrationExtension extends Extension
             $integrationDefinition->setArguments($arguments);
 
             $container->setDefinition($integration['class'], $integrationDefinition);
+
+            $factoryParameters[$key] = $integration['class']; 
         }
+
+        $factoryDefinition = new Definition(ProductIntegrationFactory::class);
+        $factoryDefinition->setArguments($factoryParameters);
+        $container->setDefinition(ProductIntegrationFactory::class, $factoryDefinition);
     }
 
     private function resolveArguments($container, array $integration): array
--- src/ProductIntegrationBundle/ProductIntegrationFactory.php
+++ src/ProductIntegrationBundle/ProductIntegrationFactory.php
@@ -4,5 +4,15 @@ namespace ProductIntegrationBundle;
 
 class ProductIntegrationFactory
 {
-    
+    private $resolvedIntegrations;
+
+    public function __construct(array $resolvedIntegrations)
+    {
+        $this->resolvedIntegrations = $resolvedIntegrations;
+    }
+
+    public function getIntegration(string $integrationName)
+    {
+        dump('HAHAHA');
+    }
 }
