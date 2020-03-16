98972f4f319f34fa211fc54ea1bdb5f4d557796e
JC Frane <john.frane@zmtsys.com>
2020-03-16 12:31:49 +0800

    [ PIW-312 ][ FEATURE ] Added condition befoe updating balance.

--- src/TransactionBundle/Manager/TransactionManager.php
+++ src/TransactionBundle/Manager/TransactionManager.php
@@ -190,18 +190,20 @@ class TransactionManager extends TransactionOldManager
         foreach ($subTransactions as $subTransaction) {
             $customerProduct = array_get($customerProducts, $subTransaction->getCustomerProduct()->getId(), $subTransaction->getCustomerProduct());
             $customerBalance = new Number(array_get($customers, $customerProduct->getCustomerID(), 0));
-            if ($subTransaction->isDeposit()) {
-                $customerProductBalance = new Number($customerProduct->getBalance());
-                $customerProductBalance = $customerProductBalance->plus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()));
-                $customerProduct->setBalance($customerProductBalance->toString());
-                $customerBalance = $customerBalance->plus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()))->toString();
-            } elseif ($subTransaction->isWithdrawal()) {
-                $customerProductBalance = new Number($customerProduct->getBalance());
-                $customerProductBalance = $customerProductBalance->minus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()));
-                $customerProduct->setBalance($customerProductBalance . '');
-                $customerBalance = $customerBalance->minus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()))->toString();
+            if (!$subTransaction->getFailedProcessingWithIntegration()) {
+                if ($subTransaction->isDeposit()) {
+                    $customerProductBalance = new Number($customerProduct->getBalance());
+                    $customerProductBalance = $customerProductBalance->plus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()));
+                    $customerProduct->setBalance($customerProductBalance->toString());
+                    $customerBalance = $customerBalance->plus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()))->toString();
+                } elseif ($subTransaction->isWithdrawal()) {
+                    $customerProductBalance = new Number($customerProduct->getBalance());
+                    $customerProductBalance = $customerProductBalance->minus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()));
+                    $customerProduct->setBalance($customerProductBalance . '');
+                    $customerBalance = $customerBalance->minus($subTransaction->getDetail('convertedAmount', $subTransaction->getAmount()))->toString();
+                }
             }
-
+            
             array_set($customerProducts, $customerProduct->getId(), $customerProduct);
             array_set($customers, $customerProduct->getCustomerID(), $customerBalance);
         }
