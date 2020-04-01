ec008c9f982878f8c9a9f635992a7bc8c955fa12
Jayfer <>
2020-04-01 13:59:18 +0800

    [PIW-376] fix locale error

--- src/ApiBundle/RequestHandler/MemberHandler.php
+++ src/ApiBundle/RequestHandler/MemberHandler.php
@@ -127,7 +127,7 @@ class MemberHandler
         return $groupPaymentOptions;
     }
 
-    public function changeMemberLocale(Request $request, Customer $member, string $locale, string $ip): array
+    public function changeMemberLocale(Request $request, Customer $member, string $locale): array
     {
         $token = $this->tokenStorage->getToken()->getToken();
         $session = $this->sessionRepository->findBySessionId($token);
