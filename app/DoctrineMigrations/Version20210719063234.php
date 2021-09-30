a2a4e94919d774152eb925e9985b5f6a408b0448
Ramon Calimbas <calimbasr@yahoo.com>
2021-09-30 16:20:20 +0800

    [PIW-535][FEATURE] - PO Integration - migration

--- app/DoctrineMigrations/Version20210719063234.php
+++ app/DoctrineMigrations/Version20210719063234.php
@@ -20,11 +20,6 @@ final class Version20210719063234 extends AbstractMigration
 	    $this->addSql('ALTER TABLE transaction ADD transaction_payment_option_type VARCHAR(255) GENERATED ALWAYS AS (transaction_other_details->>"$.paymentOptionOnTransaction.code") NULL');
 	    $this->addSql('CREATE INDEX `transaction_payment_option_type_idx` ON `transaction`(`transaction_payment_option_type`)');
 
-	    /*
-	    $this->addSql('ALTER TABLE transaction DROP CONSTRAINT FK_723705D1294149FB, DROP COLUMN transaction_payment_option_type');
-	    $this->addSql('ALTER TABLE transaction ADD transaction_payment_option_type VARCHAR(255) GENERATED ALWAYS AS (transaction_other_details->>"$.paymentOptionOnTransaction.code") NULL');
-	    $this->addSql('CREATE INDEX `transaction_payment_option_type_idx` ON `transaction`(`transaction_payment_option_type`)');
-
 	    // Country
 	    $this->addSql('ALTER TABLE customer ADD customer_country_code VARCHAR(4) AFTER customer_country_id');
 	    $this->addSql('UPDATE customer SET customer_country_code = (SELECT country_code FROM country WHERE country_id=customer_country_id)');
@@ -36,72 +31,9 @@ final class Version20210719063234 extends AbstractMigration
 	    $this->addSql('UPDATE customer SET customer_country_code = \'MK\' WHERE customer_country_code=\'YU\'');
 	    $this->addSql('UPDATE customer SET customer_country_code = \'NL\' WHERE customer_country_code=\'AN\'');
 	    $this->addSql('UPDATE customer SET customer_country_code = \'FR\' WHERE customer_country_code=\'FRM\'');
-	    */
+
 	    // Gateway
 	    $this->addSql('ALTER TABLE gateway DROP FOREIGN KEY FK_14FEDD7FDF7081C');
-
-	    /*
-	    $gatewayFees = [
-		    //ECOPAYZ
-		    ['paymentOption' => 'ecopayz', 'gatewayType' => 'merchant', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'ecopayz', 'gatewayType' => 'personal', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'ecopayz', 'gatewayType' => 'merchant', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'ecopayz', 'gatewayType' => 'personal', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    //USDT
-		    ['paymentOption' => 'usdt', 'gatewayType' => 'merchant', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'usdt', 'gatewayType' => 'personal', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'usdt', 'gatewayType' => 'merchant', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'usdt', 'gatewayType' => 'personal', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    //BITCOIN
-		    ['paymentOption' => 'bitcoin', 'gatewayType' => 'merchant', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'bitcoin', 'gatewayType' => 'personal', 'transactionType' => 'deposit', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'bitcoin', 'gatewayType' => 'merchant', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-		    ['paymentOption' => 'bitcoin', 'gatewayType' => 'personal', 'transactionType' => 'withdraw', 'rates' => [
-			    'first' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-			    'succeeding' => ['memberFeePercentage' => 0, 'companyFeePercentage' => 0],
-		    ]],
-	    ];
-
-	    $this->addSql('
-            UPDATE setting 
-            SET setting_value = JSON_SET(setting_value, "$.fees", JSON_MERGE_PRESERVE(JSON_EXTRACT(setting_value, "$.fees"), ?)) 
-            WHERE setting_code = ?',
-		    [json_encode($gatewayFees), 'gateway.fees']
-	    );
-	    */
     }
 
     public function down(Schema $schema) : void
@@ -125,7 +57,6 @@ final class Version20210719063234 extends AbstractMigration
 	    $this->addSql('ALTER TABLE customer DROP customer_country_code');
 
 	    // Gateway
-	    $this->addSql('ALTER TABLE gateway CHANGE gateway_type gateway_type VARCHAR(25) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
 	    $this->addSql('ALTER TABLE gateway ADD CONSTRAINT FK_14FEDD7FDF7081C FOREIGN KEY (gateway_payment_option) REFERENCES payment_option (payment_option_code)');
     }
 }
