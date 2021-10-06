b363a581f2135c077f675bfe88a11cf7df716496
Melvin D. Protacio <melvin.protacio@zmtsys.com>
2021-10-06 17:36:16 +0800

    comment out SERVER_NAME in fastcgi_params

--- opt/nginx/docker-entrypoint.d/40-add-event-options.sh
+++ opt/nginx/docker-entrypoint.d/40-add-event-options.sh
@@ -4,3 +4,4 @@ if [ ! -f /etc/nginx/nginx.conf.orig ]; then
 fi
 cp /etc/nginx/nginx.conf.orig /etc/nginx/nginx.conf
 sed -i 's/\(worker_connections\)\(.*\)$/\1\2\n    use epoll;\n    multi_accept on;/' /etc/nginx/nginx.conf
+sed -i 's/\(.*\)SERVER_NAME\(.*\)$/#\1SERVER_NAME\2/' /etc/nginx/fastcgi_params
